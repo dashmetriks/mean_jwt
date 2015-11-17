970b340bdf63fa925d7ce8f9ba116fda8cae4aed
Tom Slattery <tom.slattery@macys.com>
2015-11-17 15:52:58 -0800

    first commit

---
+++ public/core.js
@@ -0,0 +1,69 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http, $window, $location) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos2 = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos2 = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos2 = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+
+    $scope.login = function() {
+                        console.log("easdfdsafsdaf");
+		$http({
+			method: 'POST',
+			url: 'http://localhost:8080/authenticate',
+			data: 'name=' + $scope.user.username + '&password=' + $scope.user.password,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).success(function(data) {
+                        console.log(data);
+			$window.sessionStorage.setItem('token', data.token);
+//			$location.path('/');
+		});
+    }
+
+    $scope.addEvent = function(id) {
+        $http({
+			method: 'GET',
+			url: 'http://localhost:8080/api/adduserevent/wo9999/' + id,
+			headers: {'Content-Type': 'application/json', 'x-access-token': $window.sessionStorage.getItem('token') }
+		}).success(function(data) {
+                $scope.todos2 = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}
