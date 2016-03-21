cce5880993d629de984290c57eb8064b7bd9066e
Tom Slattery <tom.slattery@macys.com>
2016-03-21 13:58:04 -0700

    func asyn ustatus

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,8 @@ var Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({ 
     name: String, 
+    fname: String, 
+    lname: String, 
     password: String, 
     admin: Boolean 
 }));
