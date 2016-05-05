53dcd390b7af52f5ec256a98b7d0aa7032bc6165
Tom Slattery <tom.slattery@macys.com>
2016-05-05 14:41:14 -0700

    adding endpoint config

--- server.js
+++ server.js
@@ -1235,5 +1235,5 @@ app.use(function (req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 // listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(config.port_endpoint);
+console.log("App listening on port " +  config.port_endpoint);
